f5ca04760862d5ffa3d119e088f583d72be7012d
Justin Fagnani <justinfagnani@google.com>
2020-11-13 11:42:25 -0800

    format

--- schema.ts
+++ schema.ts
@@ -170,7 +170,6 @@ export interface Reference {
  */
 export interface CustomElementDeclaration extends ClassDeclaration {}
 
-
 /**
  * The additional fields that a custom element adds to classes and mixins.
  */
@@ -411,34 +410,34 @@ export interface ClassMethod extends FunctionLike {
 
 /**
  * A description of a mixin.
- * 
+ *
  * Mixins are functions which generate a new subclass of a given superclass.
  * This interfaces describes the class and custom element features that
  * are added by the mixin. As such, it extends the CustomElement interface and
  * ClassLike interface.
- * 
+ *
  * Since mixins are functions, it also extends the FunctionLike interface. This
  * means a mixin is callable, and has parameters and a return type.
- * 
+ *
  * The return type is often hard or impossible to accurately describe in type
  * systems like TypeScript. It requires generics and an `extends` operator
  * that TypeScript lacks. Therefore it's recommended that the return type is
  * left empty. The most common form of a mixin function takes a single
  * argument, so consumers of this interface should assume that the return type
  * is the single argument subclassed by this declaration.
- * 
+ *
  * A mixin should only have a superclass if it composes another mixin, and the
  * superclass should reference that mixin.
- * 
+ *
  * @example
- * 
+ *
  * This JavaScript mixin declaration:
  * ```javascript
  * const MyMixin = (base) => class extends base {
  *   foo() { ... }
  * }
  * ```
- * 
+ *
  * Is described by this JSON:
  * ```json
  * {
