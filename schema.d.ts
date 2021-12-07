80b48f19419af971fca7062a647f16ba2dc7f5e7
Benny Powers <bennypowers@users.noreply.github.com>
2021-12-07 17:48:36 +0200

    Add spread bool to Parameter (#83)

--- schema.d.ts
+++ schema.d.ts
@@ -592,6 +592,11 @@ export interface Parameter extends PropertyLike {
    * Whether the parameter is optional. Undefined implies non-optional.
    */
   optional?: boolean;
+  /**
+   * Whether the parameter is a rest parameter. Only the last parameter may be a rest parameter.
+   * Undefined implies single parameter.
+   */
+  rest?: boolean;
 }
 
 export interface FunctionLike {
